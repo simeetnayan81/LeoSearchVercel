766c62515913d53015567b4dcf68cea7cd35893c
Simeet Nayan <simeetnayan.8100@gmail.com>
2023-05-17 13:53:46 +0530

    modified files

--- api/index.py
+++ api/index.py
@@ -1,8 +1,36 @@
 
 from flask import Flask, render_template, request
-from utils.perform_search import sem_search
+
+import pinecone
+from langchain.embeddings import HuggingFaceHubEmbeddings
+
 
 app = Flask(__name__)
+
+
+PINECONE_KEY='3b757889-ed10-4965-96b9-02e967afe9f6'
+HUGGINGFACE_KEY='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'
+
+pinecone.init(api_key=PINECONE_KEY, environment="asia-southeast1-gcp-free")
+pinecone_index = pinecone.Index("leo-search")
+
+repo_id = "sentence-transformers/all-mpnet-base-v2"
+hf = HuggingFaceHubEmbeddings(
+    repo_id=repo_id,
+    task="feature-extraction",
+    huggingfacehub_api_token=HUGGINGFACE_KEY,
+)
+
+
+def sem_search(query):
+	print(query)
+	qemb=hf.embed_query(query)
+	print('d2')
+	xc = pinecone_index.query(qemb, top_k=3, include_metadata=True)
+	results=[]
+	for result in xc['matches']:
+		results.append({'title': result['metadata']['question'], 'url':'google.com'})
+	return results
 
 @app.route('/')
 def index():
